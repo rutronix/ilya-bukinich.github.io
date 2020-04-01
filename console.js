5ac056f689eaea762ee5572b8a1d2093d47d3df9
ilya-bukinich <ilya.bukinich@gmail.com>
2020-04-01 17:09:08 +0300

    upd

--- console.js
+++ console.js
@@ -1,4 +1,4 @@
-consoleText(['#StayHome', '#СидимДома'], 'text',['white']);
+consoleText(['#StayHome', '#ОставайсяДома'], 'text',['white']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
